89ae3846d496aa4cd22c875698309140a62bd5cd
firestar99 <firestar99@sydow.cloud>
2025-12-05 16:29:41 +0100

    template: turn mygraphics into a lib

--- generated/graphics/spirv-builder/mygraphics/src/lib.rs
+++ generated/graphics/spirv-builder/mygraphics/src/lib.rs
@@ -72,10 +72,6 @@
 
 pub mod ash_renderer;
 
-pub fn main() -> anyhow::Result<()> {
-    ash_renderer::main()
-}
-
 pub fn enable_debug_layer() -> bool {
     std::env::var("DEBUG_LAYER")
         .map(|e| !(e == "0" || e == "false"))
